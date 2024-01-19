2d0cb92c8276bfdf6242c34873b5a28a6b664c3a
Jack <jack@code.it>
2024-01-19 11:03:30 +0700

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Weerachai Anotaipaiboon on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Weerachai Anotaipaiboon on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
